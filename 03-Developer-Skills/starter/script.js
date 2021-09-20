6ee66b8fce145222391a63f060ea139856437a09
antara walid <antara.walid.27@gmail.com>
2021-09-20 18:40:40 +0100

    finished developer skills

---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,26 @@
+// Remember, we're gonna use strict mode in all scripts now!
+("use strict");
+
+// const x = {
+//   a: 12,
+//   b: 15,
+//   tab: [10, 20, 30],
+// };
+// console.table(x);
+
+//************ coding challenge #1 **********************
+
+// arr = [12, 5, -5, 0, 4];
+// function printForcast(arr) {
+//   let arrString = [];
+//   let givenTemps = "";
+//   for (let i = 0; i < arr.length; i++) {
+//     arrString[i] = String(arr[i]);
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     givenTemps += arrString[i] + "ºC" + " in " + String(i + 1) + " days ... ";
+//   }
+//   console.log("..." + givenTemps);
+// }
+
+// printForcast(arr);
